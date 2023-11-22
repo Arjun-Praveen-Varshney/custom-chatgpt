75f14538d17fb8e72beaa90134477e8ababcb875
Arjun <arjun.varshney1423@gmail.com>
2023-11-23 00:01:07 +0530

    fixed

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
     setquery("");
     setstartchat(true);
     setloading(true);
-    const answer = await fetch(process.env.API_URL, {
+    const answer = await fetch("https://custom-chatgpt-seven.vercel.app/api", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(query),
