1a3bf6894d84628b122d7394ea8ea6e7d80c0e1b
Arjun <arjun.varshney1423@gmail.com>
2023-11-22 23:55:16 +0530

    fixing

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
     setquery("");
     setstartchat(true);
     setloading(true);
-    const answer = await fetch(`${process.env.DOMAIN_NAME}/api`, {
+    const answer = await fetch(process.env.API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(query),
